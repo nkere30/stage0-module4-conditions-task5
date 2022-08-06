c6e26c7586ec3e0939865c826920d4d146a07372
nkere30 <ninokereselidze30@gmail.com>
2022-08-06 23:22:34 +0400

    Completed

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,29 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void isLeapYear(int year) {
+        boolean reminderBool = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        int reminder = reminderBool? 1 : 0;
+        switch (reminder) {
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+            isLeapYear(2020);
+            isLeapYear(2021);
+            isLeapYear(2024);
+            isLeapYear(2000);
+            isLeapYear(3000);
+            isLeapYear(2022);
+            isLeapYear(100);
+            isLeapYear(400);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -1,6 +1,35 @@
 package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
-    public void amountOfDays(int month) {
+    public static void amountOfDays(int month) {
+        switch (month) {
+            case 2:
+                System.out.println(28);
+                break;
+            case 4, 6, 9, 11:
+                System.out.println(30);
+                break;
+            case 1, 3, 5, 7, 8, 10, 12:
+                System.out.println(31);
+                break;
+            default:
+                System.out.println("wrong number!");
+        }
+    }
+
+    public static void main(String[] args) {
+        amountOfDays(1);
+        amountOfDays(2);
+        amountOfDays(3);
+        amountOfDays(4);
+        amountOfDays(5);
+        amountOfDays(6);
+        amountOfDays(7);
+        amountOfDays(8);
+        amountOfDays(9);
+        amountOfDays(10);
+        amountOfDays(11);
+        amountOfDays(12);
+        amountOfDays(13);
     }
 }
